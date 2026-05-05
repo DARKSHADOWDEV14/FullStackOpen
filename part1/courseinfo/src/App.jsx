4174160610b1229a4c18919f08e6927abf302c6c
Andrew <andresdavid.dev.1405@gmail.com>
2026-05-05 11:25:43 -0500

    estudiando

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,14 +1,126 @@
-import { useState } from 'react'
+// Importaciones
+import { useState } from "react";
 
 
-function App() {
- 
+//Funciones
+const Header = ({ course }) => {
+  return (
+    <>
+      <h1>{course}</h1>
+    </>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <p key={part.name}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+    </>
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return (
+    <>
+      <p>Number of exercises {totalExercises}</p>
+    </>
+  );
+};
+
+const Display = ({ counter }) => <> {counter} </>;
+
+const Button = ({ onClick, text }) => 
+  <button onClick={onClick}> {text} </button>
+
+
+// Aplicación principal
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [allClicks, setAll] = useState([])
+  // const [clicks, setClicks] = useState({left: 0, right: 0})
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
+  }
+
+  const increaseByOne = () => {
+    console.log("increasing, value before", counter);
+    setCounter(counter + 1);
+  };
+
+  const decreaseByOne = () => {
+    console.log("decreasing, value before", counter);
+    setCounter(counter - 1);
+  };
+
+  const setToZero = () => {
+    console.log("resetting to zero, value before", counter);
+    setCounter(0);
+  };
+
+
+  // setTimeout(() => setCounter(counter + 1), 1000);
+  // console.log("rendering...", counter);
+
+  const course = "Half Stack application development";
+
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <>
-      
-    </>
-  )
-}
+      <Header course={course} />
 
-export default App
+      <Content parts={parts} />
+
+      <Total parts={parts} />
+      <Display counter={counter} />
+
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+
+      <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
