7a6bcc3bab12f222046a79f30b013b7780a8859b
Andrew <andresdavid.dev.1405@gmail.com>
2026-06-14 11:11:39 -0500

    redux

---
+++ part6/unicafe-redux/src/App.jsx
@@ -0,0 +1,30 @@
+
+import { useSelector, useDispatch } from 'react-redux'
+
+const App = () => {
+  const dispatch = useDispatch()
+
+  const { good, ok, bad } = useSelector(state => state)
+
+  return (
+    <div>
+      <button onClick={() => dispatch({ type: 'GOOD' })}>
+        good
+      </button>
+
+      <button onClick={() => dispatch({ type: 'OK' })}>
+        ok
+      </button>
+
+      <button onClick={() => dispatch({ type: 'BAD' })}>
+        bad
+      </button>
+
+      <div>good {good}</div>
+      <div>ok {ok}</div>
+      <div>bad {bad}</div>
+    </div>
+  )
+}
+
+export default App
